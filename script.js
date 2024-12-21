9e15c84552ebf03c48cda5664cf5e8dfd957853f
Kashyap Das <kashyap.education2234@gmail.com>
2024-12-21 17:56:20 +0530

    Adding the loading the screen

--- script.js
+++ script.js
@@ -24,3 +24,42 @@ Shery.makeMagnet("#kashyap", {
     ease: "cubic-bezier(0.23, 1, 0.320, 1)",
     duration: 1,
   });
+// gsap implementation for black screen animation
+const tl = gsap.timeline();
+window.addEventListener("load",()=>{
+  document.querySelector("body").style.overflow = "hidden";
+  tl.to("#bar",{
+    width : "105%",
+    duration : 2.5,
+    ease : "power2.inOut"
+  },0)
+  .to(".black1",{
+    height : "100%",
+    duration : 0.8,
+    ease: "circ.out",
+  },2)
+  .to(".black2",{
+    height : "100%",
+    duration : 0.8,
+    ease: "circ.out",
+  },2)
+  tl.from("#black-heading h1",{
+    x : 1000,
+    opacity : 0,
+    duration : 1,
+    ease : "circ.out"
+  })
+  tl.to("#black-div",{
+    y : "-100%",
+    duration : 1.3,
+    delay : 0.2,
+    ease : "power4.inOut",
+    onended: ()=>{
+      document.querySelector("body").style.overflowY = "auto" 
+      setTimeout(()=>document.querySelector("#black-div").style.display = "none",900);
+    }
+  })
+})
+
+
+
